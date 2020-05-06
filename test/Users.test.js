e07d2e7c1eb47b75963db58031623f17b6e36117
Chukwuka Emi <chukwuka.emi.185926@unn.edu.ng>
2020-05-06 12:34:49 +0100

    add migration to test script

--- test/Users.test.js
+++ test/Users.test.js
@@ -24,10 +24,8 @@ describe("Users resolvers", () => {
 			`,
 			result = await graphql(schema, query, userResolvers);
 		const { data } = result;
-		console.log(JSON.parse(JSON.stringify(data)));
 		const { createUser } = data;
 		const { email } = createUser;
-		console.log(email);
 		expect(email).to.equal("emichukwuka@gmail.com");
 	});
 });
